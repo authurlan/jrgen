b3b92055b4bf87b21f1053f4b9cda5212d18bb60
abe-abe <andrew.evseeff@gmail.com>
2020-10-09 23:12:25 +0300

    for arrays printing minItems and maxItems in documentation

--- src/utils.js
+++ src/utils.js
@@ -124,10 +124,17 @@ exports.parsePropertyList = (name, schema) => {
 
   let entries = [];
 
+  var fullDescription = schema.description || "";
+  if (schema.type === "array") {
+    var min = schema.minItems === undefined ? "0" : schema.minItems;
+    var max = schema.maxItems === undefined ? "N" : schema.maxItems;
+    fullDescription += " (" + min + ".." + max + " items)";
+  }
+
   entries.push({
     name: name,
     type: schema.type,
-    description: schema.description || "",
+    description: fullDescription,
     constraints: Object.entries({
       minLength: schema.minLength,
       maxLength: schema.maxLength,
